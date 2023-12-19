2cf3cb27aae563fedfced969168e947518915d56
DongHyeok Lim <jh07050@gmail.com>
2023-12-19 18:22:12 +0900

    feat: prisma setting

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { BoardsModule } from './boards/boards.module';
+import { PrismaModule } from 'prisma/prisma.module';
 
 @Module({
-  imports: [BoardsModule],
+  imports: [PrismaModule, BoardsModule],
   controllers: [AppController],
   providers: [AppService],
 })
