8b886820481a7aa26fb888c82cc03fa38b723cab
DongHyeok Lim <jh07050@gmail.com>
2023-12-26 20:13:56 +0900

    feat: async 비동기 처리

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -2,33 +2,34 @@ import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/commo
 import { BoardsService } from './boards.service';
 import { CreateBoardDto } from './dto/create-board.dto';
 import { UpdateBoardDto } from './dto/update-board.dto';
+import { Boards } from '@prisma/client';
 
 @Controller('boards')
 export class BoardsController {
   constructor(private readonly boardsService: BoardsService) {}
 
   @Post()
-  create(@Body() createBoardDto: CreateBoardDto) {
+  async create(@Body() createBoardDto: CreateBoardDto) {
     return this.boardsService.create(createBoardDto);
   }
 
   @Get()
-  findAll() {
+  async findAll(): Promise<Boards[]> {
     return this.boardsService.findAll();
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.boardsService.findOne(+id);
+  async findOne(@Param('id') id: number): Promise<Boards> {
+    return this.boardsService.findOne(id);
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateBoardDto: UpdateBoardDto) {
+  async update(@Param('id') id: string, @Body() updateBoardDto: UpdateBoardDto): Promise<Boards> {
     return this.boardsService.update(+id, updateBoardDto);
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string) {
+  async remove(@Param('id') id: string): Promise<void> {
     return this.boardsService.remove(+id);
   }
 }
