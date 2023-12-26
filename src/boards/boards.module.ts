3bf66335286e813bce527691d282e259110c776f
DongHyeok Lim <jh07050@gmail.com>
2023-12-26 20:14:29 +0900

    feat: repository 등록

--- src/boards/boards.module.ts
+++ src/boards/boards.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { BoardsService } from './boards.service';
 import { BoardsController } from './boards.controller';
+import { BoardRepository } from './boards.repository';
 
 @Module({
   controllers: [BoardsController],
-  providers: [BoardsService],
+  providers: [BoardsService, BoardRepository],
 })
 export class BoardsModule {}
